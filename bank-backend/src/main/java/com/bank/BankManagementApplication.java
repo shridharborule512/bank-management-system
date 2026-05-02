81737d2a5c94fedd9a1287d6aa9af6e7d97af32f
Shridhar borule <shridharborule2003@gmail.com>
2026-05-02 21:48:01 +0530

    Updated account service

--- bank-backend/src/main/java/com/bank/BankManagementApplication.java
+++ bank-backend/src/main/java/com/bank/BankManagementApplication.java
@@ -9,6 +9,9 @@ public class BankManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankManagementApplication.class, args);
 		System.err.println("app started");
+		System.err.println("app started");
+
+		
 	}
 
 }
